a48ed8e7e80d293a373bad76ab4e70b8e1c0ca74
YounesBougrine00 <76845788+YounesBougrine00@users.noreply.github.com>
2022-04-11 14:21:24 +0100

    delete

--- ClientSide/src/components/signUp/OwnerSignUp.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function OwnerSignUp() {
-  return (
-    <div>OwnerSignUp</div>
-  )
-}
-
-export default OwnerSignUp
--- routes/reservationRouter.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const reservationCtrl = require('../Contoller/reservationController')
-const auth = require('../middleware/auth')
-
-router.post('/create-reservation', reservationCtrl.createReservation)
-router.post('/pull-reservations', reservationCtrl.pullReservation)
-router.post('/get-user-reservations', reservationCtrl.getUserReservations)
-
-module.exports = router
